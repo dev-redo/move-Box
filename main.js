d07222a60741404d3c8102df7c930ccbccf52f47
dev-redo <mis05041@naver.com>
2022-01-02 07:00:17 +0900

    Feat: move box html, css, js 작성

---
+++ main.js
@@ -0,0 +1,54 @@
+const $boxRow = document.querySelector('.box_row');
+const $plusButton = document.querySelector('.plus_button');
+let box = null;
+let num = 0;
+
+function createItem() {
+  const box = document.createElement('div');
+  
+  box.initialMousePos = { x: 0, y: 0 };
+  box.offset = { x: 0, y: 0 };
+  box.num = ++num;
+  box.textContent = 'BLUE' + box.num;
+
+  box.classList.add('box');
+  box.addEventListener('mousedown', enableMove);
+  $boxRow.appendChild(box);
+}
+
+function knowNum() {
+  return num++;
+}
+
+$plusButton.addEventListener('click', () => {
+  createItem();
+});
+
+const enableMove = ({ clientX, clientY, target }) => {
+  box = target;
+  box.initialMousePos.x = clientX - box.offset.x;
+  box.initialMousePos.y = clientY - box.offset.y;
+
+  document.addEventListener('mousemove', move);
+};
+
+const move = ({ clientX, clientY }) => {
+  box.offset.x = clientX - box.initialMousePos.x;
+  box.offset.y = clientY - box.initialMousePos.y;
+  box.style.transform = `translate3d(${box.offset.x}px, ${box.offset.y}px, 0)`;
+  knowColor();
+};
+
+document.addEventListener('mouseup', () => {
+  document.removeEventListener('mousemove', move);
+});
+
+const knowColor = () => {
+  if(box.getBoundingClientRect().left >= window.innerWidth / 2) {
+    box.textContent = 'Purple' + box.num;
+    box.style.backgroundColor = '#7F00FF';
+  } else {
+    box.textContent = 'BLUE' + box.num;
+    box.style.backgroundColor = '#1b69ce';
+  }
+}
